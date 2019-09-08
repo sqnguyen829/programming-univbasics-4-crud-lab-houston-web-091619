c73a0675723d73ef1c8ad3ef3c5da2c85971a022
steven nguyen <sqnguyen829@gmail.com>
2019-09-08 21:36:34 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -36,7 +36,5 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-  new_element= "totally"
-  
-  
+  array[index_number] = element
 end
